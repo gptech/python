453127278a558ec7338d6c7682d03c00705f1e46
Strelik <jason@jrdoppes.com>
2015-01-14 23:23:07 -0500

    initial commit of robot kitty fsm with pseudocode

---
+++ robot_cat_fsm.py
@@ -0,0 +1,54 @@
+__author__ = 'jason'
+import sys
+from random import randint
+from time import clock
+
+
+# #==============================================================
+"""Create the states of the robot kitty can possibly be in.  """
+
+##==============================================================
+"""Create the transition class and initialize as object"""
+
+##==============================================================
+"""
+    Create a simple finite state machine class and initialize it into a default object
+    that describes how states are set, stored and changed to. The engine if you will.
+"""
+
+
+##==============================================================
+
+"""
+    create a character object based on that finite state machine above and give it an initial state.
+    This will be used to create the robot kitty.
+"""
+
+
+def main():
+    ##===============================================================
+    """Main entry point for the script."""
+
+    """
+    create a robot kitty variable based on your character object and assign it some states and transitions
+    for the fsm to work with. Remember the fsm you made does not already contain the states, it merely
+    defines how to work with what gets passed through to it in order to change states.
+
+    """
+
+
+
+    ##===============================================================
+    """
+    write some code that has robot kitty pass through transitions and states based on random time intervals
+    with if else logic
+    """
+
+
+##===============================================================
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
